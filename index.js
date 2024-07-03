06d8684665eab512a8ddc265fc3a583ebd780c2d
Shakil Ahmed <shakil.pasimistic@gmail.com>
2024-07-03 23:14:55 +0600

    Added expressjs initial server setup

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get('/status', function (req, res) {
+    res.json({
+        code: 200,
+        success: true,
+        message: "OK"
+    })
+});
+
+const port = process.env.PORT || 3000;
+const environment = process.env.NODE_ENV || "development";
+
+app.listen(port, () => {
+  console.log(`Server running in ${environment} mode on port ${port}`);
+});
