98affd303d71e7b7a14b80e9954946d57590aff8
Shakil Ahmed <shakil.pasimistic@gmail.com>
2024-07-04 21:52:39 +0600

    Now tests will be exist once ran successfully

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.get('/status', function (req, res) {
 const port = process.env.PORT || 3000;
 const environment = process.env.NODE_ENV || "development";
 
-app.listen(port, () => {
-  console.log(`Server running in ${environment} mode on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running in ${environment} mode on port ${port}`);
+// });
 
 export default app;
